be13aa9c08c1934b2e3f38a560f55ca6851b20e4
Kevin Chen <kvn.c8888@gmail.com>
2025-08-24 23:41:22 -0400

    Next app

--- homepage/src/app/layout.tsx
+++ homepage/src/app/layout.tsx
@@ -24,12 +24,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-      <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=arrow_outward"
-            />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
