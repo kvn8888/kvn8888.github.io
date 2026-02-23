c0893b573f7e5eea12e4019c749bf89263257b52
Kevin Chen <kvn.c8888@gmail.com>
2026-02-22 18:43:53 -0800

    Update homepage build script and fix HTML entity in bio text

--- homepage/src/app/page.tsx
+++ homepage/src/app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="pt-24">
       <h1 className="text-4xl font-bold text-center" style={{ fontFamily: 'var(--font-noto-serif)' }}>Welcome to <span className="text-[#D97757]">KevinC.dev</span></h1>
-      <p className="mt-6 text-center text-lg text-gray-700 max-w-2xl mx-auto" style={{ fontFamily: 'var(--font-geist-sans)' }}>I'm a software engineering student at the Rochester Institute of Technology. I'm currently on an internship at Ivalua, Inc. Here are some of my projects:</p>
+      <p className="mt-6 text-center text-lg text-gray-700 max-w-2xl mx-auto" style={{ fontFamily: 'var(--font-geist-sans)' }}>I&apos;m a software engineering student at the Rochester Institute of Technology. I&apos;m currently on an internship at Ivalua, Inc. Here are some of my projects:</p>
       
       <div className="mt-10 flex flex-col items-center gap-4">
         <a href="#" className="w-80 px-8 py-3 border-2 border-[#D97757] text-[black] font-medium hover:bg-[#D97757] hover:text-white transition-colors text-center">
