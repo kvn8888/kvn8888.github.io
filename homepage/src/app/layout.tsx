64f4b34648819b46cc6c4b32d0f7900a0ccdee69
Kevin Chen <kvn.c8888@gmail.com>
2026-02-23 21:55:33 -0800

    Remove redundant eslint-disable comment for Google Fonts

--- homepage/src/app/layout.tsx
+++ homepage/src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* eslint-disable-next-line @next/next/no-page-custom-font, @next/next/google-font-display */}
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional" />
       </head>
       <body
